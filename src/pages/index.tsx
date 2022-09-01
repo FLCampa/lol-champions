7395a926910b85829e49596e35362889c2117dc5
FLCampa <flcampanelli@gmail.com>
2022-09-01 14:15:54 -0300

    fix: request error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,16 @@
 // Components
+import Head from "next/head";
 import Layout from "../components/Layout";
 
 export default function Home() {
   return (
     <>
+      <Head>
+        <meta
+          httpEquiv="Content-Security-Policy"
+          content="upgrade-insecure-requests"
+        />
+      </Head>
       <Layout />
     </>
   );
